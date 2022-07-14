29e97db2ff2ad243609624ece1c9c29470d5c4d2
Blaevoet Benoit <blaevoet.benoit@outlook.com>
2022-07-14 14:36:38 +0200

    Alternative de l'exercice 2.1

--- js/2.1.js
+++ js/2.1.js
@@ -1,5 +1,8 @@
-let isMajor = prompt("What is your age ?");
-if (isMajor >= 18){
+//let isMajor = prompt("What is your age ?");
+let isMajor = confirm("Are you at least 18?")
+//if (isMajor >= 18)
+if(isMajor)
+{
     alert("You are an adult");
 }else{
     alert("You are not yet an adult");
